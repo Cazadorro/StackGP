42f1f0a2bade99102519997c1a42c79a1fdba45b
Shae Bolt <snb4y4@mst.edu>
2017-05-15 18:54:38 -0500

    finished selection.py

---
+++ combinedsampling.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+from aliassampling import weighted_choice_generator
+from resevoirsampling import weighted_reservoir_sample
+
+
+def n_choice_sampler(sample_list: list, weight_list: list, n: int, *,
+                     replacement: bool):
+    if replacement:
+        alias_generator = weighted_choice_generator(sample_list, weight_list)
+        return [next(alias_generator) for _ in range(n)]
+    else:
+        return weighted_reservoir_sample(sample_list, weight_list, n)
+
+
+def replacement_sampler(sample_list: list, weight_list: list, n: int):
+    alias_generator = weighted_choice_generator(sample_list, weight_list)
+    return [next(alias_generator) for _ in range(n)]
+
+
+def no_replacement_sampler(sample_list: list, weight_list: list, n: int):
+    return weighted_reservoir_sample(sample_list, weight_list, n)
--- selection.py
+++ selection.py
@@ -2,6 +2,8 @@
 import random
 from functools import wraps
 from operator import attrgetter, itemgetter
+from combinedsampling import n_choice_sampler, replacement_sampler, \
+    no_replacement_sampler
 
 
 def selection_decorator(func):
@@ -13,29 +15,15 @@ def selection_decorator(func):
     return selection_with_assertion
 
 
-def selection_assertion(n, individual_list, with_replacement):
+def selection_assertion(individual_list, n, with_replacement):
     assert n <= len(individual_list) or with_replacement, \
         "Error, can't return more individuals than the " \
         "population with out replacement"
 
 
-def n_elitist(gp_stack_list, num_returned):
-    sorted_stacks = sorted(gp_stack_list, key=attrgetter('fitness'),
-                           reverse=True)
-    return sorted_stacks[:num_returned]
-
-
-def stochastic_parent_selection(gp_stack_list, num_children):
-    parents1 = []
-    parents2 = []
-    for i in range(num_children):
-        parents1.append(random.choice(gp_stack_list))
-        parents2.append(random.choice(gp_stack_list))
-    return parents1, parents2
-
-
+# TODO should probably change these all to functors
 # TODO, need with/without replacement, and ability to select for worst or best
-def n_elitst(individual_list, n, *, get_worst, with_replacement):
+def n_elitist(individual_list, n, *, get_worst, with_replacement):
     selection_assertion(n, individual_list, with_replacement)
     selected = []
     individual_list.sort(reverse=get_worst)
@@ -47,17 +35,164 @@ def n_elitst(individual_list, n, *, get_worst, with_replacement):
     return selected
 
 
-def fitness_proportional(individual_list, n, *, get_worst, with_replacement):
-    selection_assertion(n, individual_list, with_replacement)
+def fitness_proportional(individual_list, n, *, get_worst, replacement):
+    selection_assertion(n, individual_list, replacement)
+    normalized_fitness = [individual.fitness for individual in individual_list]
+    fitness_sum = sum(normalized_fitness)
+    if get_worst:
+        normalized_fitness = [1 - (fitness / fitness_sum) for fitness in
+                              normalized_fitness]
+    else:
+        normalized_fitness = [(fitness / fitness_sum) for fitness in
+                              normalized_fitness]
+    return n_choice_sampler(individual_list, normalized_fitness, n,
+                            replacement=replacement)
 
-    pass
+
+def stochastic_selection(individual_list, n, *, get_worst, replacement):
+    selection_assertion(n, individual_list, replacement)
+    if replacement:
+        return random.choices(individual_list, k=n)
+    return random.sample(individual_list, n)
 
 
-def stochastic_selection(individual_list, n, *, get_worst, with_replacement):
-    selection_assertion(n, individual_list, with_replacement)
-    pass
+def k_tournament_selection(individual_list, n, k, *, get_worst, replacement):
+    selection_assertion(n, individual_list, replacement)
+
+    fitness_selector = min if get_worst else max
+    selected = []
+    if replacement:
+        for tournament_round in range(n):
+            contestants = [individual_list[random.randrange(n)] for _ in
+                           range(k)]
+            selected.append(
+                fitness_selector(contestants, key=lambda x: x.fitness))
+
+    else:
+        index_dictionary = {i: individual for i, individual in
+                            enumerate(individual_list)}
+        for tournament_round in range(n):
+            contestant_keys = random.choice(list(index_dictionary.keys()), k=k)
+            selected_key = fitness_selector(contestant_keys,
+                                            key=lambda x: index_dictionary[
+                                                x].fitness)
+            selected.append(index_dictionary.pop(selected_key))
 
 
-def k_tournament_selection(individual_list, n, *, get_worst, with_replacement):
-    selection_assertion(n, individual_list, with_replacement)
-    pass
+class Selector:
+    def __init__(self, get_worst: bool, replacement: bool):
+        self.get_worst = get_worst
+        self.replacement = replacement
+
+    def __call__(self, individual_list: list, n: int):
+        selection_assertion(individual_list, n, self.replacement)
+
+
+class ElitistSelector(Selector):
+    def __call__(self, individual_list: list, n: int):
+        super().__call__(individual_list, n)
+        selected = []
+        individual_list.sort(reverse=self.get_worst)
+        selections_left = n
+        while selections_left:
+            n_selected = selections_left % len(individual_list)
+            selected.extend(individual_list[:n_selected])
+            selections_left -= n_selected
+        return selected
+
+
+class FitnessProportionalSelector(Selector):
+    def __init__(self, get_worst: bool, replacement: bool):
+        super().__init__(get_worst, replacement)
+        if get_worst:
+            self.fitness_normalizer = self.worst_normalized
+        else:
+            self.fitness_normalizer = self.best_normalized
+        if replacement:
+            self.sampler = self.replacement_sample
+        else:
+            self.sampler = self.no_replacement_sample
+
+    @staticmethod
+    def worst_normalized(fitness_sum, fitnesses):
+        return [1 - (fitness / fitness_sum) for fitness in fitnesses]
+
+    @staticmethod
+    def best_normalized(fitness_sum, fitnesses):
+        return [(fitness / fitness_sum) for fitness in fitnesses]
+
+    @staticmethod
+    def replacement_sample(individual_list: list, n: int, normalized_fitnesses):
+        return replacement_sampler(individual_list, normalized_fitnesses, n)
+
+    @staticmethod
+    def no_replacement_sample(individual_list: list, n: int,
+                              normalized_fitnesses):
+        return no_replacement_sampler(individual_list, normalized_fitnesses, n)
+
+    def __call__(self, individual_list: list, n: int):
+        super().__call__(individual_list, n)
+        normalized_fitness = [individual.fitness for individual in
+                              individual_list]
+        fitness_sum = sum(normalized_fitness)
+        normalized_fitnesses = self.fitness_normalizer(fitness_sum,
+                                                       normalized_fitness)
+        return self.sampler(individual_list, n, normalized_fitnesses)
+
+
+class StochasticSelector(Selector):
+    def __init__(self, get_worst: bool, replacement: bool):
+        super().__init__(get_worst, replacement)
+        if replacement:
+            self.sampler = self.replacement_sample
+        else:
+            self.sampler = self.no_replacement_sample
+
+    @staticmethod
+    def replacement_sample(individual_list: list, n: int):
+        return random.choices(individual_list, k=n)
+
+    @staticmethod
+    def no_replacement_sample(individual_list: list, n: int):
+        return random.sample(individual_list, n)
+
+    def __call__(self, individual_list: list, n: int):
+        super().__call__(individual_list, n)
+        return self.sampler(individual_list, n)
+
+
+class TournamentSelector(Selector):
+    def __init__(self, get_worst: bool, replacement: bool, k: int):
+        super().__init__(get_worst, replacement)
+        self.fitness_selector = min if get_worst else max
+        if replacement:
+            self.sampler = self.replacement_sample
+        else:
+            self.sampler = self.no_replacement_sample
+        self.k = k
+
+    def replacement_sample(self, individual_list: list, n: int):
+        selected = []
+        for tournament_round in range(n):
+            contestants = [individual_list[random.randrange(n)] for _ in
+                           range(self.k)]
+            selected.append(
+                self.fitness_selector(contestants, key=lambda x: x.fitness))
+        return selected
+
+    def no_replacement_sample(self, individual_list: list, n: int):
+        selected = []
+        index_dictionary = {i: individual for i, individual in
+                            enumerate(individual_list)}
+        for tournament_round in range(n):
+            contestant_keys = random.choice(list(index_dictionary.keys()),
+                                            k=self.k)
+            selected_key = self.fitness_selector(contestant_keys,
+                                                 key=lambda x: index_dictionary[
+                                                     x].fitness)
+            selected.append(index_dictionary.pop(selected_key))
+        return selected
+
+    def __call__(self, individual_list: list, n: int):
+        super().__call__(individual_list, n)
+        return self.sampler(individual_list, n)
